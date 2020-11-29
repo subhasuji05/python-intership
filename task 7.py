1de7f28cf97c98faba918f28465945b851fe7ca1
subhasuji05 <63350626+subhasuji05@users.noreply.github.com>
2020-11-29 23:50:35 +0530

    Add files via upload

---
+++ task 7.py
@@ -0,0 +1,24 @@
+#EXERCISE 1
+import day7ex1moD
+a=10
+b=25
+c=day7ex1mod.add(a,b)
+d=day7ex1mod.sub(a,b)
+print("sum of addition:",c)
+print("sum of subtraction:",d)
+
+#EXERCISE2
+import pandas as pd
+s = pd.Series([2, 4, 6, 8, 10])
+print(s)
+
+#EXERCISE 3
+import random
+print(random.randrange(1,100))
+
+#EXERCISE 4
+import sys
+print (sys.path)
+
+#EXERCISE 5
+Installed and uninstalled an package using pip
